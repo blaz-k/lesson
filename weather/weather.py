7ba3358727a7b23bcf96a1e032bc9c8ee78cbb7c
blaz <blazyy@gmail.com>
2021-04-12 17:59:26 +0200

    no message

--- weather/weather.py
+++ weather/weather.py
@@ -1,20 +1,18 @@
 import json
-
+from datetime import datetime
 
 with open("weather.json") as weather_file:
     weather_stat = json.load(weather_file)
 
-print(weather_stat)
+timestamp_sunrise = weather_stat["sys"]["sunrise"]
+sunrise = "sunrise: " + str(datetime.fromtimestamp(timestamp_sunrise))
 
-for items in weather_stat:
-    city = (weather_stat["name"])
-    country_code = (weather_stat["cod"])
-    current_temperature = (weather_stat["main"]["temp"])
-    weather_description = (weather_stat["weather"][0]["description"])
-    #print(city, country_code, current_temperature, weather_description)
-    weather_txt = "In {0} with code of {1}, is current temperature of {2} with {3}".format(items.get("city"),
-                                                                                       items.get("country_code"),
-                                                                                       items.get("current_temperature"),
-                                                                                       items.get("weather_description"))
-    print(weather_txt)
-    break
+timestamp_sunset = weather_stat["sys"]["sunset"]
+sunset = "sunset :" + str(datetime.fromtimestamp(timestamp_sunset))
+
+city = weather_stat["name"]
+country_code = weather_stat["cod"]
+current_temperature = weather_stat["main"]["temp"]
+weather_description = weather_stat["weather"][0]["description"]
+print(city, country_code, current_temperature, weather_description, sunrise, sunset)
+
