12278e1f4ee71ebfec3286310e6ee5721c45040d
blaz <blazyy@gmail.com>
2021-04-16 08:59:30 +0200

    no message

---
+++ millionaire/millionaire.py
@@ -0,0 +1,11 @@
+print("Welcome in ----- WHO WANT'S TO BE A MILLIONAIRE--- show!")
+
+
+name = input("Tell me your name: ").capitalize()
+print(name + ", welcome in this hot seat today!")
+
+
+#questions:
+
+#first question
+
