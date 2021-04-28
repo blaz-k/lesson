197e62f58101f91c4c3ef339213a211017064a47
blaz <blazyy@gmail.com>
2021-04-28 11:58:34 +0200

    no message

---
+++ 17/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.hml")
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
