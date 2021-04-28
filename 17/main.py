1f7861e5f7e333197466d26d06a1b18118a9c5e1
blaz <blazyy@gmail.com>
2021-04-28 13:10:52 +0200

    no message

--- 17/main.py
+++ 17/main.py
@@ -6,25 +6,37 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    existing_secret = request.cookies.get("secret")
+    response = make_response(render_template("index.html"))
+
+    if not existing_secret:
+        secret = randint(1, 10)
+        response.set_cookie("secret", str(secret))
+
+    return response
 
 
 @app.route("/result", methods=["POST"])
 def result():
-    secret = randint(1, 30)
-    guess = int(request.form.get("guess"))
 
+    guess = int(request.form.get("guess"))
+    secret = int(request.cookies.get("secret"))
     if guess == secret:
         result = "Correct"
 
         response = make_response(render_template("result.html", result=result))
+
+        secret = randint(1, 30)
+        response.set.cookie("secret", str(secret))
         return response
+
     elif guess < secret:
         result = "too SMALL"
     elif guess > secret:
         result = "too BIG"
 
     response = make_response(render_template("result.html", result=result))
+
     return response
 
 
