de32a02c943d30dcff1f3ec8b15bc5923717f3ac
blaz <blazyy@gmail.com>
2021-05-14 12:56:58 +0200

    no message

--- 20.a/main.py
+++ 20.a/main.py
@@ -11,6 +11,9 @@ db = SQLAlchemy(db_url)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
+    first_name = db.Column(db.String, unique=False)
+    last_name = db.Column(db.String, unique=False)
+    address = db.Column(db.String, unique=False)
     password = db.Column(db.String, unique=False)
     session_token = db.Column(db.String, unique=False)
 
@@ -21,6 +24,22 @@ app = Flask(__name__)
 db.create_all()
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    session_cookie = request.cookies.get("session")
+    if session_cookie:
+        user = db.query(User).filter_by(session_token=session_cookie).first()
+        if user:
+            return render_template("dashboard.html", user=user)
+
+    return "You are not logged in!!"
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     session_cookie = request.cookies.get("session")
@@ -29,36 +48,6 @@ def home():
         if user:
             return render_template("index.html", user=user)
     return render_template("index.html")
-
-
-@app.route("/registration", methods=["GET", "POST"])
-def registration():
-    if request.method == "GET":
-        return render_template("registration.html")
-
-    elif request.method == "POST":
-        #get all the names
-        email = request.form.get("user-email")
-        password = request.form.get("password")
-        repeat = request.form.get("repeat")
-
-        # ce ga se ni mormo nardit novega userja in preverit ce je v bazi
-
-        existing_user = db.query(User).filter_by(email=email).first()
-
-        if existing_user:
-            return "This user already exists!"
-        else:
-        #preveriti moramo ce se passworda ujemata
-            if password == repeat:
-                password_hash = sha256(password.encode("utf-8")).hexdigest()
-                # moramo zakamuflirat password
-                new_user = User(email=email, password=password_hash)
-                new_user.save()
-            else:
-                return "Passwords do not match!"
-
-    return redirect(url_for("home"))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -84,6 +73,39 @@ def login():
 
         else:
             return "Username or password is not correct"
+
+    return redirect(url_for("home"))
+
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == "GET":
+        return render_template("registration.html")
+
+    elif request.method == "POST":
+        #get all the names
+        email = request.form.get("user-email")
+        password = request.form.get("password")
+        repeat = request.form.get("repeat")
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        address = request.form.get("address")
+
+        # ce ga se ni mormo nardit novega userja in preverit ce je v bazi
+
+        existing_user = db.query(User).filter_by(email=email).first()
+
+        if existing_user:
+            return "This user already exists!"
+        else:
+        #preveriti moramo ce se passworda ujemata
+            if password == repeat:
+                password_hash = sha256(password.encode("utf-8")).hexdigest()
+                # moramo zakamuflirat password
+                new_user = User(email=email, password=password_hash, first_name=first_name, last_name=last_name, address=address)
+                new_user.save()
+            else:
+                return "Passwords do not match!"
 
     return redirect(url_for("home"))
 
