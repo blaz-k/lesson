e6cfbc228ec0233f683b79041a7dbd73637b96be
blaz <blazyy@gmail.com>
2021-05-18 11:21:33 +0200

    no message

---
+++ 20.a/tests/test_main.py
@@ -0,0 +1,47 @@
+import os
+import pytest
+
+# important: this line needs to be set BEFORE the "app" import
+os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+
+from main import app, db
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+
+    cleanup()  # clean up before every test
+
+    db.create_all()
+
+    yield client
+
+
+def cleanup():
+    # clean up/delete the DB (drop all tables in the database)
+    db.drop_all()
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert b"Do you want to register or log-in" in response.data
+
+
+
+def test_registration_page_post_get(client):
+    response = client.get("/registration")
+
+    assert b"Welcome to registration" in response.data
+
+
+def test_registration_page_post(client):
+    response = client.post("/registration", data={"user-email": "b@b", "password": "b", "repeat": "b"})
+
+
+def test_dashboard_page(client):
+    client.post("/registration", data={"user-email": "b@b", "password": "b", "repeat": "b"})
+
+    response = client.get("/dashboard")
+
+    assert b"Your email is" in response.data
