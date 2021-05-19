82852b9e717dc2f439c57276dff162dacaa8c41e
blaz <blazyy@gmail.com>
2021-05-19 13:50:40 +0200

    no message

--- 20.a/main.py
+++ 20.a/main.py
@@ -4,6 +4,7 @@ from sqla_wrapper import SQLAlchemy
 from hashlib import sha256
 import uuid
 import requests
+from secret import api_key
 
 db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
 db = SQLAlchemy(db_url)
