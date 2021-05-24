b9526448ff2deebc7ba526ff610d8c4e194365e4
blaz <blazyy@gmail.com>
2021-05-24 09:41:26 +0200

    no message

---
+++ final-project/main.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+# in alphabetical order
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
